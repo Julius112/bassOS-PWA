b32e461fb9b741b6e5bd5e5a9112354ef5f9dfcf
Julius Niedworok <julius.n@gmx.net>
2018-08-22 08:56:07 +0000

    Adding Onsen UI framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { OnsenModule } from 'ngx-onsenui';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 import { AppComponent } from './app.component';
 
@@ -7,8 +10,12 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OnsenModule
   ],
   providers: [],
   bootstrap: [AppComponent]
